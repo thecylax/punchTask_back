58ba9671d69d95e36a99c37f483bfe69d756415c
Robson Cardoso dos Santos <rcs09@c3sl.ufpr.br>
2015-04-09 14:40:23 -0300

    updated the view
    Signed-off-by: Robson Cardoso dos Santos <rcs09@c3sl.ufpr.br>

--- punchTaskApp/contributors/views.py
+++ punchTaskApp/contributors/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 
 from punchTaskApp.contributors.forms import ContributorForm
+from punchTaskApp.contributors.models import Contributor
 
 def contributor_new(request, template='contributors/contributor_new.html'):
     if request.method == 'POST':
@@ -12,11 +12,17 @@ def contributor_new(request, template='contributors/contributor_new.html'):
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
             # Create the User record
             user = User(username=username, email=email)
             user.set_password(password)
             user.save()
             # Create Contributor Record
+            city = form.cleaned_data['city']
+            state = form.cleaned_data['state']
+            cont = Contributor(city=city, state=state, user_rec=user)
+            cont.save()
             # Process payment (via Stripe)
             # Auto login the user
             return HttpResponseRedirect('/success/')
