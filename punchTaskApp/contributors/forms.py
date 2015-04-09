92c4c0a1c58e2bb4764dde77afce6606cb941f03
Robson Cardoso dos Santos <rcs09@c3sl.ufpr.br>
2015-04-09 14:34:21 -0300

    updated the form
    Signed-off-by: Robson Cardoso dos Santos <rcs09@c3sl.ufpr.br>

--- punchTaskApp/contributors/forms.py
+++ punchTaskApp/contributors/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
-class ContributorForm(UserCreationForm):
+class AddressMixin(forms.ModelForm):
+    class Meta:
+        model = Contributor
+        fields = ('city', 'state',)
+        widgets = {'city': forms.TextInput(attrs={'class':'form-control'}),
+                   'state': forms.TextInput(attrs={'class':'form-control'}),
+                   }
+    
+class ContributorForm(AddressMixin, UserCreationForm):
+    first_name = forms.CharField(required = True, widget = forms.TextInput(attrs={'class':'form-control'}))
+    last_name = forms.CharField(required = True, widget = forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(
         required = True,
         widget = forms.TextInput(attrs={'class':'form-control'})
