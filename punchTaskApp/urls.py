71d6c4cde3f0f385ab290bd6bcb9b4e4126b8d2e
Robson Cardoso dos Santos <rcs09@c3sl.ufpr.br>
2015-04-12 12:10:26 -0300

    create login & logout URLs
    Signed-off-by: Robson Cardoso dos Santos <rcs09@c3sl.ufpr.br>

--- punchTaskApp/urls.py
+++ punchTaskApp/urls.py
@@ -6,6 +6,8 @@ from punchTaskApp.home.views import HomePage
 urlpatterns = patterns('',
     # Initial page
                        url(r'^$', HomePage.as_view(), name='home'),
+                       url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+                       url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/login/'}),
                        url(r'^signup/$',
                            'punchTaskApp.contributors.views.contributor_new', name = 'cont_new'
                        ),
