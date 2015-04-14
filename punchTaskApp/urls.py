3492205050ea659d1c9c84973ba4cfb5f0bfdb23
Robson Cardoso dos Santos <rcs09@c3sl.ufpr.br>
2015-04-14 08:19:18 -0300

    added new task url
    Signed-off-by: Robson Cardoso dos Santos <rcs09@c3sl.ufpr.br>

--- punchTaskApp/urls.py
+++ punchTaskApp/urls.py
@@ -10,10 +10,10 @@ urlpatterns = patterns('',
                        url(r'^$', HomePage.as_view(), name='home'),
                        url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
                        url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/login/'}),
-                       url(r'^signup/$',
-                           'punchTaskApp.contributors.views.contributor_new', name = 'cont_new'
-                       ),
+                       url(r'^signup/$', 'punchTaskApp.contributors.views.contributor_new', name = 'cont_new'),
+                       
                        url(r'^task/list/$', TaskList.as_view(), name='task_list'),
+                       url(r'^task/new/$', 'punchTaskApp.tasks.views.task_cru', name='task_new'),
                        url(r'^task/(?P<uid>[\w-]+)/', include(task_urls)),
                        
                        (r'^admin/', include(admin.site.urls)),
