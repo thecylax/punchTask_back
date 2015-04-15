c299bad55cfe813da621313ab6d0ea5424ea692b
Robson Cardoso dos Santos <rcs09@c3sl.ufpr.br>
2015-04-15 13:23:55 -0300

    created the edit form
    Signed-off-by: Robson Cardoso dos Santos <rcs09@c3sl.ufpr.br>

--- punchTaskApp/tasks/urls.py
+++ punchTaskApp/tasks/urls.py
@@ -4,4 +4,7 @@ task_urls = patterns('',
                      url(r'^$',
                          'punchTaskApp.tasks.views.task_detail', name='task_detail'
                      ),
+                     url(r'^edit/$',
+                         'punchTaskApp.tasks.views.task_cru', name='task_update'
+                     ),
             )
