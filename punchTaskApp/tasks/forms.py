8765a8af28e7610b82fa44f72a72adbb6bab6b83
Robson Cardoso dos Santos <rcs09@c3sl.ufpr.br>
2015-04-15 12:56:34 -0300

    modified new task view to allow for edits
    Signed-off-by: Robson Cardoso dos Santos <rcs09@c3sl.ufpr.br>

--- punchTaskApp/tasks/forms.py
+++ punchTaskApp/tasks/forms.py
@@ -5,6 +5,7 @@ from punchTaskApp.tasks.models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
+        fields = ('name', 'desc',)
         widgets = {
             'name': forms.TextInput(
                 attrs={
--- punchTaskApp/tasks/views.py
+++ punchTaskApp/tasks/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseForbidden, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
@@ -39,20 +39,34 @@ def task_detail(request, uid):
     return render(request, 'tasks/task_detail.html', context)
 
 @login_required()
-def task_cru(request):
+def task_cru(request, uid=None):
+    if uid:
+        task = get_object_or_404(Task, uid=uid)
+        if task.owner != request.user:
+            return HttpResponseForbidden()
+    else:
+        task = Task(owner=request.user)
+        
     if request.POST:
-        form = TaskForm(request.POST)
+        form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             task = form.save(commit=False)
+            # usar try exception DoesNotExist
+            max_ui = Task.objects.latest('uid').uid
+            task.uid = max_ui + 1
             task.owner = request.user
+            print task.uid
             task.save()
             redirect_url = reverse('punchTaskApp.tasks.views.task_detail', args=(task.uid,))
             
             return HttpResponseRedirect(redirect_url)
     else:
-        form = TaskForm()
+        form = TaskForm(instance=task)
 
-    context = {'form': form,}
+    context = {
+        'form': form,
+        'task': task,
+    }
     template = 'tasks/task_cru.html'
     
     return render(request, template, context)
